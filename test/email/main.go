5cf82942020a5d1cb704fa4f79f59813a873c8b6
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-09-18 18:30:23 -0600

    add test main, update taskfile
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

---
+++ test/email/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+	"github.com/theopenlane/newman"
+	"github.com/theopenlane/riverboat/test/common"
+
+	"github.com/theopenlane/riverboat/pkg/jobs"
+)
+
+// the main function here will insert an email job into the river
+// this will be picked up by the river server and processed
+// assuming the server is run in the default setup, the email will be written to a file (fixtures/email)
+func main() {
+	client := common.NewInsertOnlyRiverClient()
+
+	msg := newman.NewEmailMessageWithOptions(
+		newman.WithSubject("test subject"),
+		newman.WithText("body"),
+		newman.WithTo([]string{"meow@example.com"}),
+	)
+
+	_, err := client.Insert(context.Background(), jobs.EmailArgs{
+		Message: *msg,
+	}, nil)
+	if err != nil {
+		log.Fatal().Err(err).Msg("error inserting email job")
+	}
+}
