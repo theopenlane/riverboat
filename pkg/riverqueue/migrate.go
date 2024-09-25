dcfc2b3e76db1f0e9fa28197ce597e6784c241d4
Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>
2024-09-25 13:59:25 -0600

    feat: add interface for insert only client
    Signed-off-by: Sarah Funkhouser <147884153+golanglemonade@users.noreply.github.com>

--- pkg/riverqueue/migrate.go
+++ pkg/riverqueue/migrate.go
@@ -1,19 +1,21 @@
-package river
+package riverqueue
 
 import (
 	"context"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
 	"github.com/riverqueue/river/rivermigrate"
 )
 
-// runMigrations runs the migrations for the river server
+// RunMigrations runs the migrations for the river server
 // see https://riverqueue.com/docs/migrations for more information
-func runMigrations(ctx context.Context, dbPool *pgxpool.Pool) error {
+func RunMigrations(ctx context.Context, dbPool *pgxpool.Pool) error {
 	// run migrations here
-	migrator := rivermigrate.New(riverpgxv5.New(dbPool), nil)
+	migrator, err := rivermigrate.New(riverpgxv5.New(dbPool), nil)
+	if err != nil {
+		return err
+	}
 
 	if _, err := migrator.Migrate(ctx, rivermigrate.DirectionUp, nil); err != nil {
 		return err
