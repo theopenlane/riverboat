7556407e0f881a39b296594e6f235ed059b5ba22
Lanre Adelowo <yo@lanre.wtf>
2026-01-19 23:23:20 +0100

    add comment

--- config/queue.go
+++ config/queue.go
@@ -1,8 +1,12 @@
 package config
 
 const (
-	QueueAnalytics    = "analytics"
-	QueueCompliance   = "compliance"
-	QueueTrustcenter  = "trustcenter"
+	// QueueAnalytics is the queue for analytics jobs.
+	QueueAnalytics = "analytics"
+	// QueueCompliance is the queue for compliance jobs.
+	QueueCompliance = "compliance"
+	// QueueTrustcenter is the queue for trust center jobs.
+	QueueTrustcenter = "trustcenter"
+	// QueueNotification is the queue for notification jobs.
 	QueueNotification = "notifications"
 )
