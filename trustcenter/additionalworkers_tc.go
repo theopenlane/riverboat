e3e48ac516cd967d5d69079d8f4fc0ac63670c6c
Lanre Adelowo <yo@lanre.wtf>
2026-04-02 18:21:51 +0100

    task generate

--- trustcenter/additionalworkers_tc.go
+++ trustcenter/additionalworkers_tc.go
@@ -57,8 +57,8 @@ func AddConditionalWorkers(workers *river.Workers, w Workers, insertOnlyClient *
 		log.Info().Msg("worker enabled: attest NDA request")
 	}
 
-	if w.CreatePreviewDomainWorker.Config.Enabled {
-		if err := setAndValidateOpenlaneConfigDefaults(&w.CreatePreviewDomainWorker.Config.OpenlaneConfig, w.OpenlaneConfig); err != nil {
+	if w.CreatePreviewDomainAcmeWorker.Config.Enabled {
+		if err := setAndValidateOpenlaneConfigDefaults(&w.CreatePreviewDomainAcmeWorker.Config.OpenlaneConfig, w.OpenlaneConfig); err != nil {
 			log.Error().Err(err).Msg("failed to set and validate openlane config defaults for preview domain acme worker")
 			return nil, err
 		}
@@ -79,6 +79,7 @@ func AddConditionalWorkers(workers *river.Workers, w Workers, insertOnlyClient *
 		}
 
 		w.CreateDomainScanWorker.WithRiverClient(insertOnlyClient)
+
 		if err := river.AddWorkerSafely(workers, &w.CreateDomainScanWorker); err != nil {
 			return nil, err
 		}
@@ -96,7 +97,7 @@ func AddConditionalWorkers(workers *river.Workers, w Workers, insertOnlyClient *
 			return nil, err
 		}
 
-		log.Info().Msg("worker enabled: create domain scan")
+		log.Info().Msg("worker enabled: retrieve domain scan results")
 	}
 
 	// add more workers here
