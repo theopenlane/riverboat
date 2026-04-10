ec17a340bea273cbc43f33517e8af2d4341afb49
Lanre Adelowo <yo@lanre.wtf>
2026-04-11 00:25:32 +0100

    add periodic worker

--- trustcenter/periodicjobs_tc.go
+++ trustcenter/periodicjobs_tc.go
@@ -10,8 +10,15 @@ import (
 	"github.com/theopenlane/core/common/jobspec"
 )
 
-// minInterval prevents spamming from a bag config
-var minInterval = 1 * time.Minute
+const (
+	// minInterval prevents spamming from a bag config
+	minInterval = 1 * time.Minute
+
+	// atleast one day
+	minOrgDeleteReminderInterval = 1
+
+	twentyFourhours = time.Hour * 24
+)
 
 // CreatePeriodicJobs creates periodic jobs for the trust center module
 func CreatePeriodicJobs(c Workers) ([]*river.PeriodicJob, error) {
@@ -33,9 +40,26 @@ func CreatePeriodicJobs(c Workers) ([]*river.PeriodicJob, error) {
 			&river.PeriodicJobOpts{RunOnStart: true},
 		)
 		jobs = append(jobs, validateCustomDomainJobs)
+
+		log.Info().Msg("periodic worker enabled: validate custom domain")
 	}
 
 	if c.OrganizationDeletionReminderWorker.Config.Enabled {
+		days := c.OrganizationDeletionReminderWorker.Config.PaymentMethodInterval
+		if days < minOrgDeleteReminderInterval {
+			days = minOrgDeleteReminderInterval
+		}
+
+		orgDeletionReminders := river.NewPeriodicJob(
+			river.PeriodicInterval(time.Duration(days)*twentyFourhours),
+			func() (river.JobArgs, *river.InsertOpts) {
+				return jobspec.OrganizationDeletionReminderArgs{}, nil
+			},
+			&river.PeriodicJobOpts{RunOnStart: true},
+		)
+		jobs = append(jobs, orgDeletionReminders)
+
+		log.Info().Msg("periodic worker enabled: organization deletion reminder")
 	}
 
 	return jobs, nil
