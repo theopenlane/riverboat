485a4857e315a000e0e40bf404e555e9e115270c
Lanre Adelowo <yo@lanre.wtf>
2026-09-24 20:23:03 +0100

    sanitize html in csv exports' (#317)

--- pkg/jobs/mocks/mocks.go
+++ pkg/jobs/mocks/mocks.go
@@ -16,10 +16,19 @@ func NewMockPDFRenderer(t interface {
 	mock.TestingT
 	Cleanup(func())
 }) *MockPDFRenderer {
+	if helper, ok := t.(interface{ Helper() }); ok {
+		helper.Helper()
+	}
+
 	mock := &MockPDFRenderer{}
 	mock.Mock.Test(t)
 
-	t.Cleanup(func() { mock.AssertExpectations(t) })
+	t.Cleanup(func() {
+		if helper, ok := t.(interface{ Helper() }); ok {
+			helper.Helper()
+		}
+		mock.AssertExpectations(t)
+	})
 
 	return mock
 }
